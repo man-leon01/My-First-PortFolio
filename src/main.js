24cf6b6b9e446862058b9c661ed1c60d9b89daac
man-leon01 <manleon115@gmail.com>
2025-09-17 09:32:01 +0000

    modification et responsive sur le Header, construction de Aboutet commencement de Contact me

--- src/main.js
+++ src/main.js
@@ -1,2 +1,33 @@
-import './style.css'
+import './css/style.css'
+import './css/about.css'
+import './css/projects.css'
+import './css/contact.css'
+import './css/footer.css'
+
+const avail = document.querySelector(".point-available");
+// ICON MENU
+const iconMenu = document.querySelector(".icon-menu");
+const iconClose = document.querySelector(".icon-close");
+const navLinks = document.querySelector(".navlinks");
+const netWorks = document.querySelector(".networks");
+
+
+setInterval(() => {
+    avail.classList.toggle("avail");
+}, 450);
+
+iconMenu.addEventListener("click", () =>{
+    console.log("clicked");
+    navLinks.style.display = "flex";
+    iconMenu.style.display = "none";
+    iconClose.style.display = "block";
+    netWorks.style.display = "flex";
+})
+iconClose.addEventListener("click", () =>{
+    navLinks.style.display = "none";
+    iconMenu.style.display = "block";
+    iconClose.style.display = "none";
+    netWorks.style.display = "none";
+})
+
 
